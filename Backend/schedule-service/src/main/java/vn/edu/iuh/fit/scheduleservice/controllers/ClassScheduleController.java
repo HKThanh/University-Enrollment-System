1a884699d5ec0a471ca52b67c89d2d4b84c00947
HKThanh <21086351.thanh@student.iuh.edu.vn>
2025-02-25 09:32:29 +0700

    BackEnd/Schedule-service: add function to check conflict shedule in ClassScheduleController

--- Backend/schedule-service/src/main/java/vn/edu/iuh/fit/scheduleservice/controllers/ClassScheduleController.java
+++ Backend/schedule-service/src/main/java/vn/edu/iuh/fit/scheduleservice/controllers/ClassScheduleController.java
@@ -3,6 +3,7 @@ package vn.edu.iuh.fit.scheduleservice.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import vn.edu.iuh.fit.scheduleservice.dtos.QueryClassSchedule;
 import vn.edu.iuh.fit.scheduleservice.dtos.ResponseWrapper;
 import vn.edu.iuh.fit.scheduleservice.models.ClassSchedule;
 import vn.edu.iuh.fit.scheduleservice.services.ClassScheduleService;
@@ -38,6 +39,27 @@ public class ClassScheduleController {
                         HttpStatus.OK.value()
                 )
         );
+    }
+
+    private boolean isConflict(QueryClassSchedule existingSchedule, QueryClassSchedule newSchedule) {
+        // Check if the schedules are on the same day of the week
+        if (existingSchedule.schedule().getDayOfWeek() != newSchedule.schedule().getDayOfWeek()) {
+            return false;
+        }
+
+        // Split the time slots into start and end times
+        String[] existingTime = existingSchedule.schedule().getTimeSlot().split("-");
+        String[] newTime = newSchedule.schedule().getTimeSlot().split("-");
+
+        // Check if the schedules overlap in time
+        if (Integer.parseInt(existingTime[0]) < Integer.parseInt(newTime[1]) &&
+                Integer.parseInt(existingTime[1]) > Integer.parseInt(newTime[0])) {
+            // If the schedules overlap in time, check if they also overlap in date
+            return existingSchedule.schedule().getStartDate().before(newSchedule.schedule().getEndDate()) &&
+                    existingSchedule.schedule().getEndDate().after(newSchedule.schedule().getStartDate());
+        }
+
+        return false;
     }
 
 }
