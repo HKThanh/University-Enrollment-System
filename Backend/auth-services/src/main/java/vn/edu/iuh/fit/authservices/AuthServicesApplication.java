e4aef6b12c1e0c1c05c16d2575c00e83044d54f7
LeMinhThat <mthat456@gmail.com>
2025-03-02 16:22:28 +0700

    done authservices

--- Backend/auth-services/src/main/java/vn/edu/iuh/fit/authservices/AuthServicesApplication.java
+++ Backend/auth-services/src/main/java/vn/edu/iuh/fit/authservices/AuthServicesApplication.java
@@ -2,8 +2,10 @@ package vn.edu.iuh.fit.authservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthServicesApplication {
 
 	public static void main(String[] args) {
