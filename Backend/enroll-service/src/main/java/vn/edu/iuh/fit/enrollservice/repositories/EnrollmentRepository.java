ba5e5a285d057e5efa49c68d84f4a70d3ef1075a
HKThanh <21086351.thanh@student.iuh.edu.vn>
2025-05-16 19:50:50 +0700

    fix change class in enroll-service

--- Backend/enroll-service/src/main/java/vn/edu/iuh/fit/enrollservice/repositories/EnrollmentRepository.java
+++ Backend/enroll-service/src/main/java/vn/edu/iuh/fit/enrollservice/repositories/EnrollmentRepository.java
@@ -23,8 +23,14 @@ public interface EnrollmentRepository extends JpaRepository<Enrollment, Enrollme
                       @Param("p_class_id") String p_class_id,
                       @Param("p_group_id") int p_group_id);
 
-    @Procedure(procedureName = "change_class")
-    int changeClass(String p_student_id, String p_old_class_id, String p_new_class_id, int p_group_id);
+    // @Procedure(procedureName = "change_class")
+    // int changeClass(String p_student_id, String p_old_class_id, String p_new_class_id, int p_group_id);
+
+    @Query(nativeQuery = true, value = "SELECT change_class(:p_student_id, :p_old_class_id, :p_new_class_id, :p_group_id)")
+    int changeClass(@Param("p_student_id") String p_student_id, 
+                    @Param("p_old_class_id") String p_old_class_id, 
+                    @Param("p_new_class_id") String p_new_class_id, 
+                    @Param("p_group_id") int p_group_id);
 
     @Query("SELECT e FROM Enrollment e WHERE e.studentId = :studentId AND e.semester = :semester AND e.year = :year")
     List<Enrollment> findEnrollmentsIncludingSemesterAndYear(String studentId, int semester, int year);
